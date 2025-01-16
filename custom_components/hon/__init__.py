3325bb27b9dac91e8568f22ea01bda808a1970ce
Niek Schoemaker <32094562+niekschoemaker@users.noreply.github.com>
2025-01-16 13:52:48 +0100

    Correctly call on_update function with hass.add_job so it runs on the correct thread

--- custom_components/hon/__init__.py
+++ custom_components/hon/__init__.py
@@ -48,7 +48,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     coordinator: DataUpdateCoordinator[dict[str, Any]] = DataUpdateCoordinator(
         hass, _LOGGER, name=DOMAIN
     )
-    hon.subscribe_updates(coordinator.async_set_updated_data)
+    hon.subscribe_updates(lambda data: hass.add_job(coordinator.async_set_updated_data, data))
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.unique_id] = {"hon": hon, "coordinator": coordinator}
